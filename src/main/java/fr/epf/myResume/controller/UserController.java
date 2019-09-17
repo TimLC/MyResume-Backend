fac6fb8af465f17c3132bc2f86651c5b324fbc0e
Pol frx <pol.2frx@hotmail.fr>
2019-09-17 14:45:42 +0200

    add update user

--- src/main/java/fr/epf/myResume/controller/UserController.java
+++ src/main/java/fr/epf/myResume/controller/UserController.java
@@ -4,6 +4,8 @@ import fr.epf.myResume.DAO.UserDAO;
 import fr.epf.myResume.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/user")
@@ -19,5 +21,10 @@ public class UserController {
     public User getUser() {
         int id = 1;
         return userDAO.findById(Long.valueOf(id)).get();
+    }
+
+    @PostMapping("/update")
+    public void updateUser(@RequestBody User user) {
+        this.userDAO.save(user);
     }
 }

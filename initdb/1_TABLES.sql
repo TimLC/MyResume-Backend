7d7094ddfec0d61f5fae1b82929e8a55dc04b323
Pol frx <pol.2frx@hotmail.fr>
2019-09-20 12:19:56 +0200

    sql scipts

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,12 +1,18 @@
+create table person
+(
+    id bigint auto_increment,
+    constraint users_pk
+        primary key (id),
+    `full_name` TEXT not null,
+    `job_title` TEXT not null,
+    `id_github` TEXT not null
+);
 create table users
 (
     id bigint auto_increment,
     constraint users_pk
         primary key (id),
     username TEXT not null,
-    full_name TEXT not null,
-    job_title TEXT not null,
-    id_github TEXT not null,
     password TEXT not null
 );
 create table experiences
@@ -15,8 +21,8 @@ create table experiences
     constraint experiences_pk
         primary key (id),
     company TEXT not null,
-    begin_date TEXT not null,
-    end_date TEXT not null,
+    `begin_date` TEXT not null,
+    `end_date` TEXT not null,
     address TEXT not null,
     description TEXT not null
 );
@@ -25,24 +31,24 @@ create table skills
     id bigint auto_increment,
     constraint skills_pk
         primary key (id),
-    skill_name TEXT not null
+    `skill_name` TEXT not null
 );
 create table hobbies
 (
     id bigint auto_increment,
     constraint hobbies_pk
         primary key (id),
-    hobby_name TEXT not null
+    `hobby_name` TEXT not null
 );
 create table projects
 (
     id bigint auto_increment,
     constraint projects_pk
         primary key (id),
-    project_name TEXT not null
-    begin_date TEXT not null
-    end_date TEXT not null
-    description TEXT not null
-    git_link TEXT not null
+    `project_name` TEXT not null,
+    `begin_date` TEXT not null,
+    `end_date` TEXT not null,
+    description TEXT not null,
+    `git_link` TEXT not null
 );
 

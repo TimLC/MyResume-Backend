980cb2f8c33e28ee44f421e08758af5eeb5bf43d
Pol frx <pol.2frx@hotmail.fr>
2019-09-21 17:05:40 +0200

    update user to add password hashing

--- src/main/java/fr/epf/myResume/controller/UserController.java
+++ src/main/java/fr/epf/myResume/controller/UserController.java
@@ -2,6 +2,7 @@ package fr.epf.myResume.controller;
 
 import fr.epf.myResume.DAO.UserDAO;
 import fr.epf.myResume.entities.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +16,34 @@ public class UserController {
         this.userDAO = userDAO;
     }
 
-    @GetMapping
-    public User getUser() {
+    @PostMapping
+    public Boolean connexion(@RequestBody User sendUser) {
         int id = 1; //just one user
-        return userDAO.findById(Long.valueOf(id)).get();
+        User user = userDAO.findById(Long.valueOf(id)).get();
+        return user.canLogin(sendUser.getPassword(), sendUser.getUsername());
     }
 
     @PutMapping("/update")
     public void updateUser(@RequestBody User user) {
+        int id = 1; //just one user
+        user.setId(Long.valueOf(id));
+        user.hashPassword();
+        userDAO.save(user);
+    }
+
+    @PutMapping("/update/username")
+    public void updateUserName(@RequestBody User sendUser) {
+        int id = 1; //just one user
+        User user = userDAO.findById(Long.valueOf(id)).get();
+        user.setUsername(sendUser.getUsername());
+        userDAO.save(user);
+    }
+
+    @PutMapping("/update/password")
+    public void updateUserPassword(@RequestBody User sendUser) {
+        int id = 1; //just one user
+        User user = userDAO.findById(Long.valueOf(id)).get();
+        user.setNewPassword(sendUser.getPassword());
         this.userDAO.save(user);
     }
 }
--- src/main/java/fr/epf/myResume/entities/User.java
+++ src/main/java/fr/epf/myResume/entities/User.java
@@ -1,19 +1,15 @@
 package fr.epf.myResume.entities;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class User {
     private Long id;
     private String username;
-    private String full_name;
-    private String job_title;
-    private String id_github;
     private String password;
 
-    public User(Long id, String username, String full_name, String job_title, String id_github, String password) {
+    public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
-        this.full_name = full_name;
-        this.job_title = job_title;
-        this.id_github = id_github;
         this.password = password;
     }
 
@@ -33,35 +29,27 @@ public class User {
         this.username = username;
     }
 
-    public String getFull_name() {
-        return full_name;
-    }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
-    }
-
-    public String getJob_title() {
-        return job_title;
-    }
-
-    public void setJob_title(String job_title) {
-        this.job_title = job_title;
-    }
-
-    public String getId_github() {
-        return id_github;
-    }
-
-    public void setId_github(String id_github) {
-        this.id_github = id_github;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void hashPassword() {
+        BCryptPasswordEncoder b = new BCryptPasswordEncoder(12);
+        this.password = b.encode(this.password);
+    }
+
+    public void setNewPassword(String password) {
+        BCryptPasswordEncoder b = new BCryptPasswordEncoder(12);
+        password = b.encode(password);
+        this.password = password;
+    }
+
+    public Boolean canLogin(String rawPassword, String username) {
+        BCryptPasswordEncoder b = new BCryptPasswordEncoder(12);
+        return b.matches(rawPassword, this.password) && username.equals(this.username);
     }
 }

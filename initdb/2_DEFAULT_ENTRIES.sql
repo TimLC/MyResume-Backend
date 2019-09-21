e92357a1fe5baeaedceef541bd49a4e98b009a54
Pol frx <pol.2frx@hotmail.fr>
2019-09-21 19:02:05 +0200

    fix sql script

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -1,6 +1,6 @@
 INSERT INTO defaultdb.persons (id, fullName, jobTitle, idGithub, phoneNumber, emailAddress) VALUES (1, 'Pol FRX', 'software engineer', 'PolFRX', '0660090127', 'pol@epfedu.fr');
 INSERT INTO defaultdb.users (id, username, password) VALUES (1, 'admin', 'admin');
-INSERT INTO defaultdb.experiences (id, company, begin_date, endDate, address, description) VALUES (1, 'Google', '16/09/2019', '17/09/2019', 'Paris', 'pute');
+INSERT INTO defaultdb.experiences (id, company, beginDate, endDate, address, description) VALUES (1, 'Google', '16/09/2019', '17/09/2019', 'Paris', 'pute');
 INSERT INTO defaultdb.skills (id, skillName) VALUES (1, 'Spring Boot');
 INSERT INTO defaultdb.skills (id, skillName) VALUES (2, 'J2EE');
 INSERT INTO defaultdb.hobbies (id, hobbyName) VALUES (1, 'Reading');

08f996ccb24cff8e678abf61f257bdd1ec4d92b4
Pol frx <pol.2frx@hotmail.fr>
2019-09-23 00:29:56 +0200

    change password user in sql script

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -1,5 +1,5 @@
 INSERT INTO defaultdb.persons (id, fullName, jobTitle, idGithub, phoneNumber, emailAddress) VALUES (1, 'Pol FRX', 'software engineer', 'PolFRX', '0660090127', 'pol@epfedu.fr');
-INSERT INTO defaultdb.users (id, username, password) VALUES (1, 'admin', 'admin');
+INSERT INTO defaultdb.users (id, username, password) VALUES (1, 'admin', '$2a$12$VkUfTyyFxmISz.w0wekZy.HZI2L89nLtGhsJ5bXShPWtavUP60F02');
 INSERT INTO defaultdb.experiences (id, company, beginDate, endDate, address, description) VALUES (1, 'Google', '16/09/2019', '17/09/2019', 'Paris', 'reve');
 INSERT INTO defaultdb.skills (id, skillName) VALUES (1, 'Spring Boot');
 INSERT INTO defaultdb.skills (id, skillName) VALUES (2, 'J2EE');

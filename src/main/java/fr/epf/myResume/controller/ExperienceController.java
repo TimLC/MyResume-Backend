4b82349d309e72ea5070f8dbbf133f340875e689
Pol frx <pol.2frx@hotmail.fr>
2019-09-24 15:34:26 +0200

    delete new cross origin policy

--- src/main/java/fr/epf/myResume/controller/ExperienceController.java
+++ src/main/java/fr/epf/myResume/controller/ExperienceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/experiences")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @Api(description="API for operations on experiences.")
 public class ExperienceController {
     private final ExperienceDAO experienceDAO;
